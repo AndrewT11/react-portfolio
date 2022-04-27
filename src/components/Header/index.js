f9806261923f3e9d6f4130ea57aa1a9f04465acd
Andrew Tran <tranandrew@gmail.com>
2022-04-27 18:25:25 -0500

    change way of exporting in Header.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ import Portfolio from "../pages/Portfolio";
 import Contact from "../pages/Contact";
 import "./header.css";
 
-export default function Header() {
+function Header() {
   const [currentPage, setCurrentPage] = useState("Home");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
@@ -39,3 +39,5 @@ export default function Header() {
     </div>
   );
 }
+
+export default Header;
