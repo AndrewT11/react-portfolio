ef7c5a22f6d95788a3632e6eec440cc05f531a36
Andrew Tran <tranandrew@gmail.com>
2022-04-27 16:38:52 -0500

    delete some boilerplate react. Home/index.js built

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./home.css";
+import Me from "./images/headshotnobg.png";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="home-left">
+        <div className="home-left-wrapper">
+          <h2 className="home-greeting">Salutations, you've stumbled upon</h2>
+          <h1 className="home-name">
+            Andrew Tran's <br />
+            Portfolio Page
+          </h1>
+          <div className="home-title">
+            <div className="home-title-wrapper">
+              <div className="home-title-jobs">Aspiring Web Developer</div>
+              <div className="home-title-jobs2">Problem Solver</div>
+              <div className="home-title-jobs3">Dog Father</div>
+              <div className="home-title-jobs4">Basketball Junkie</div>
+              <div className="home-title-jobs5">Sci-fi Nerd</div>
+            </div>
+          </div>
+          <div className="home-desc">
+            Full-stack web developer especially interested in creating React
+            websites and applications.
+          </div>
+        </div>
+      </div>
+      <div className="home-right">
+        <img src={Me} alt="" className="home-image"></img>
+      </div>
+    </div>
+  );
+}
